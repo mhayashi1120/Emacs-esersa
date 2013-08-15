382e21153bd2d6c31b61d5999e743bd57764b380
Masahiro Hayashi <mhayashi1120@gmail.com>
2013-08-15 19:31:13 +0900

    fix

--- rsa.el
+++ rsa.el
@@ -1,13 +1,11 @@
 ;;; rsa.el --- Encrypt/Decrypt string with RSA key.
 
 ;; Author: Masahiro Hayashi <mhayashi1120@gmail.com>
-;; Keywords: data play
+;; Keywords: data
 ;; URL: http://github.com/mhayashi1120/Emacs-cipher/raw/master/rsa.el
 ;; Emacs: GNU Emacs 22 or later
 ;; Version: 0.0.2
 ;; Package-Requires: ()
-
-(defconst rsa-version "0.0.2")
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
